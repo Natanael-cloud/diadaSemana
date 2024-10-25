94423a192b1b2a72313f7fa3d4271b7de9cf7fc1
natanaelcloud <natanpaivacosta@gmail.com>
2024-10-25 20:11:11 -0300

    adicionar case do 0 ao 6

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ let dia = parseInt(prompt("Digite um dia do ano:"));
 
 let resto = (dia%7);
 
-if(resto ==1){
-  alert('Terça-Feira')
+switch(resto){
+  case 1 : 
+  alert("Terça-Feira");
+  break;
+
+  case 2 : 
+  alert("Quarta-Feira");
+  break;
+
+  case 3 : 
+  alert("Quinta-Feira");
+  break;
+
+  case 4 : 
+  alert("Sexta-Feira");
+  break;
+
+  case 5 : 
+  alert("Sábado");
+  break;
+
+  case 6 : 
+  alert("Domingo");
+  break;
+
+  case 0 : 
+  alert("Segunda-Feira");
+  break;
+
 }
